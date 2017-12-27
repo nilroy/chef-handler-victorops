6b852aaf7d7ae7e220582546abef984c2c207959
Nilanjan Roy <nilroy@users.noreply.github.com>
2017-12-27 19:26:02 +0200

    Updated copyright information

--- lib/chef/handler/victorops.rb
+++ lib/chef/handler/victorops.rb
@@ -1,4 +1,4 @@
-# Copyright:: 2011, Mathieu Sauve-Frankel <msf@kisoku.net>
+# Copyright:: 2017, Nilanjan Roy <nilanjan1.roy@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
